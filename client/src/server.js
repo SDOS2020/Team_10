92c59deeb4e51897d2ad7470d390b1f870a345e6
raghav-kukreti <raghav.kukreti@gmail.com>
2020-11-12 17:32:44 +0530

    [client] Basic homepage + converted server to Express

--- client/src/server.js
+++ client/src/server.js
@@ -1,17 +1,43 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
+import bodyParser from 'body-parser'
+import express from 'express'
+import session from 'express-session'
+
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const app = express()
+
+app.use(session({
+    secret: 'stealthisifuwant',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(bodyParser.json())
+
+app.use(express.static('static'))
+
+app.use(sapper.middleware({
+    session: (req, res) => {
+        res.setHeader('cache-control', 'no-cache, no-store')
+        const sessionObj = {
+            email: req.session.email,
+            key: req.session.key,
+            error: req.session.error,
+            userType: req.session.userType,
+            recommended: req.session.recommended,
+            project: req.session.project
+        }
+        req.session.error = undefined
+        return sessionObj
+    }
+}))
+app.listen({ port: PORT },
+    () => console.log(`Server ready at http://localhost:${PORT}`)
+)
