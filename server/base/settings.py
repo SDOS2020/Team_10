280f339e88a48b4badfda62fdccbb8b10d7d41eb
raghav-kukreti <raghav.kukreti@gmail.com>
2020-11-12 19:59:49 +0530

    [server] djoser token authentication works

--- server/base/settings.py
+++ server/base/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'users.apps.UsersConfig'
 ]
 
@@ -86,7 +88,11 @@ DATABASES = {
     } 
 }
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
