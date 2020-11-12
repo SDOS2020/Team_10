6f64a83eea47b843f27123f184fae5bf9db2ebfe
raghav-kukreti <raghav.kukreti@gmail.com>
2020-11-12 19:38:03 +0530

    [server] REST working

--- server/base/urls.py
+++ server/base/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from users.views import check_server
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('check/', check_server)
 ]
