d040fd6aaf742d663d32a53bb58a59a6742b7ab8
Nivedit Mishra <nivedit17073@iiitd.ac.in>
2020-12-06 00:56:31 +0530

    [client] Added Sidebar and Newsfeed frontend for class

---
+++ client/src/routes/class/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Sidebar from '/home/tilloo/Desktop/Team_6/client/src/components/Sidebar.svelte'
+    import Newsfeed from '/home/tilloo/Desktop/Team_6/client/src/components/Newsfeed.svelte'
+
+</script>
+
+<style>
+    
+</style>
+    
+    <svelte:head>
+        <title>WebMP : Class</title>
+    </svelte:head>
+
+    <Sidebar />
+    <Newsfeed />
+    
+  
+    
+ 
+    
